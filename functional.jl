6fc89deab9fda273d912224ae49e5cc6b56c74a8
Olivier Cots <olivier.cots@irit.fr>
2024-10-30 14:23:28 +0100

    foo

--- functional.jl
+++ functional.jl
@@ -1,12 +1,8 @@
 ocp = Model()                                   # empty optimal control problem
-
 time!(ocp, t0=0, tf=1)                          # initial and final times
 state!(ocp, 2)                                  # dimension of the state
 control!(ocp, 1)                                # dimension of the control
-
 constraint!(ocp, :initial; val=[ -1, 0 ])       # initial condition
 constraint!(ocp, :final;   val=[  0, 0 ])       # final condition
-
 dynamics!(ocp, (x, u) -> [ x[2], u ])           # dynamics of the double integrator
-
 objective!(ocp, :lagrange, (x, u) -> 0.5u^2)    # cost in Lagrange form
